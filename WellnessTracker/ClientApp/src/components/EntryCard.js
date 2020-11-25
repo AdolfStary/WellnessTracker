d79a21a6d21cccbba3fe2b190421fdde2d2773fd
AdolfStary <adolf.stary@hotmail.com>
2020-11-25 16:52:16 -0700

    ADDED exercise length to page, UPDATED CSS;

--- WellnessTracker/ClientApp/src/components/EntryCard.js
+++ WellnessTracker/ClientApp/src/components/EntryCard.js
@@ -22,22 +22,13 @@ const EntryCard = (props) => {
     <Link to="/EntryDetail" className="entry-card-link" onClick={() => assignClickedEntry()}>
         <div className={`entry-card ${props.entry.entryCategory.name}`}>
 
-            <div className={`card-head ${props.entry.entryCategory.name}`}>{props.entry.entryCategory.name}
+            <div className={`card-head ${props.entry.entryCategory.name} ${props.entry.entryStatus.isPositive}`}>
+                {props.entry.entryCategory.name}
                 <div className="time">{date} - {time}</div>
+                <div className={`status`}>{props.entry.entryStatus.name}</div>
             </div>
 
-            <div className="card-body">
-                <div className={`status ${props.entry.entryStatus.name}`}>{props.entry.entryStatus.name}</div>
-                { 
-                    // If person is a diabetic and they measured blood glucose or took insulin - show diabetic data
-                    (sessionStorage.getItem('isDiabetic') === "true" && (props.entry.bg !== 0 || props.entry.insulin !== 0)) ?
-                    <div className="diabetes">
-                        <div className="bg">{props.entry.bg}</div>
-                        <div className="insulin">{props.entry.insulin}u</div>
-                    </div>
-                    : false
-                }
-                
+            <div className="card-body">                
                 {
                     // If entry was a meal, show nutrition
                     (props.entry.entryCategory.name === "Meal") ? 
@@ -45,8 +36,25 @@ const EntryCard = (props) => {
                             <div className="fats">F: {props.entry.fats}g</div>
                             <div className="carbs">C: {props.entry.carbs}g</div>
                             <div className="protein">P: {props.entry.protein}g</div>                        
-                        </div> 
+                        </div>
                         : false
+                }
+                {
+                    // If entry was a meal, show nutrition
+                    (props.entry.entryCategory.name === "Exercise") ? 
+                        <div className="exercise">
+                            <div>{props.entry.exerciseLength !== 0 ? props.entry.exerciseLength+"m" : "N/A"}</div>                      
+                        </div>
+                        : false
+                }
+                { 
+                    // If person is a diabetic and they measured blood glucose or took insulin - show diabetic data
+                    (sessionStorage.getItem('isDiabetic') === "true" && (props.entry.bg !== 0 || props.entry.insulin !== 0)) ?
+                    <div className="diabetes">
+                        <div className="bg">BG: {props.entry.bg}</div>
+                        <div className="insulin">Dose: {props.entry.insulin}u</div>
+                    </div>
+                    : false
                 }
             </div>
         </div>
