1821e95693a8402a82c86122472c65a1c814633e
AdolfStary <adolf.stary@hotmail.com>
2020-12-03 15:05:54 -0700

    ADDED Disclaimer to registration and home;

--- WellnessTracker/ClientApp/src/components/Home.js
+++ WellnessTracker/ClientApp/src/components/Home.js
@@ -8,6 +8,7 @@ export class Home extends Component {
       <div>
         <h1>WellnessTracker</h1>
             <p>Welcome, my name is Adolf Stary and I created this wellness tracker for personal use as a capstone project for TechCareers - Full Stack developer program.</p>
+            <p>This app was designed solely as an aid for personal wellness tracking. It is not intended or recommended to be used for making any diet or medical decisions. Always consult your medical practitioner or dietitian when making any decisions related to your health or diet, always use tools provided by your diabetes team or medical equipment supplier to keep track of your diabetes related data.</p>
             <p>App was built using following technologies:</p>
         <ul>
           <li><a href='https://get.asp.net/'>ASP.NET Core</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code</li>
--- WellnessTracker/ClientApp/src/components/Register.js
+++ WellnessTracker/ClientApp/src/components/Register.js
@@ -9,29 +9,35 @@ const Register = () => {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const [isDiabetic, setIsDiabetic] = useState(false);
+    const [confirmTerm, setConfirmTerm] = useState(false);
 
     const handleSubmit = (event) => {
         event.preventDefault();
         let userID = uuid();
         let encodedPassword = username + password;
 
-        axios(
-            {
-                method: 'post',
-                url: 'API/Register',
-                params: {
-                    id: userID,
-                    username: username,
-                    password:  encodedPassword,
-                    isDiabetic: isDiabetic
+        if (confirmTerm){
+            axios(
+                {
+                    method: 'post',
+                    url: 'API/Register',
+                    params: {
+                        id: userID,
+                        username: username,
+                        password:  encodedPassword,
+                        isDiabetic: isDiabetic
+                    }
                 }
+            ).then((res) => {
+                setResponse(res.data);
+                if (res.data === "Success!") window.location = "/Login";
             }
-        ).then((res) => {
-            setResponse(res.data);
+            ).catch((err) => {
+                setResponse(err.response.data);
+            });
         }
-        ).catch((err) => {
-            setResponse(err.response.data);
-        });
+        else setResponse("You need to read and understand the disclaimer before proceeding.");
+
     }
 
     
@@ -50,7 +56,12 @@ const Register = () => {
                             <label htmlFor='isDiabetic'>Include diabetes data: </label>
                             <input id='isDiabetic' name='isDiabetic' type='checkbox' onChange={(e) => setIsDiabetic(!isDiabetic)} value={isDiabetic} />
                         </div>
-                        <input type='submit' value='Register' className='btn btn-primary'/>
+                        <div className="disclaimer">
+                            <label htmlFor='confirm-term'>This app was designed solely as an aid for personal wellness tracking. It is not intended or recommended to be used for making any diet or medical decisions. Always consult your medical practitioner or dietitian when making any decisions related to your health or diet, always use tools provided by your diabetes team or medical equipment supplier to keep track of your diabetes related data. By checking this box, you confirm that you have read and understand this disclaimer:</label>
+                            <input id='confirm-term' name='confirm-term' type='checkbox' onChange={(e) => setConfirmTerm(!confirmTerm)} value={confirmTerm} />
+                            <strong></strong>
+                        </div>
+                        <input type='submit' value='Register' className='btn btn-primary' />
                     </form>
                 </div>
             </>
